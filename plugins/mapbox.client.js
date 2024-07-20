f1580ddb3c811410efe9a3266649cd550340d513
ankitpansuria <ankit.pansuria@anviya.in>
2024-07-20 12:28:00 +0530

    Initial commit

--- plugins/mapbox.client.js
+++ plugins/mapbox.client.js
@@ -4,9 +4,8 @@ import mapbox from 'mapbox-gl/dist/mapbox-gl';
 const mbxClient = require('@mapbox/mapbox-sdk')
 const mbxGeoCoding = require('@mapbox/mapbox-sdk/services/geocoding')
 
-export default async function ({ $axios,$config }, inject) {
-    const CHARTMAT_MAPBOX_AUTH_TOKEN = await $axios.$get('/api/getENV/CHARTMAT_MAPBOX_AUTH_TOKEN');
-    const baseClient = new mbxClient({ accessToken: CHARTMAT_MAPBOX_AUTH_TOKEN })
+export default function ({ $config }, inject) {
+    const baseClient = new mbxClient({ accessToken: $config.CHARTMAT_MAPBOX_AUTH_TOKEN })
     const geocodeClient = mbxGeoCoding(baseClient)
     inject('geocode', geocodeClient)
     inject('mapbox', mapbox)
